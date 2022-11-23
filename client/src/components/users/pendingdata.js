5375029224c605379a062415427622c57ad3de85
ShreyParikh1002 <shreyparikh1002@gmail.com>
2022-11-23 20:57:42 +0530

    final

--- client/src/components/users/pendingdata.js
+++ client/src/components/users/pendingdata.js
@@ -1,12 +1,7 @@
 export default [
     {
         LtNumber: "1",
-        userName: "Lessonfdsssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssaddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddds",
-        date:"10/12/dsaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa22"
-    },
-    {
-        LtNumber: "1",
-        userName: "Life ",
+        userName: "Liwqeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeesdddddddddddddeeeeeeeeeeeeeeeeeeeeeeeeeesdddddddddddddddddddddddddddfe ",
         date:"10/12/22"
     },
     {
